16ad2095864bb66d9d65f1002eb48fae7313042e
xj9@sunshinegardens.org <xj9@sunshinegardens.org>
2019-10-27 18:41:30 +0000

    copy config from rikako
    FossilOrigin-Name: ba2b7476f4ccb42f9782a370a4f25827fb72e50527dea130523f4f3fe42516ab

--- config.def.h
+++ config.def.h
@@ -1,17 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Noto Mono:size=11" };
+static const char dmenufont[]       = "Noto Mono:size=11";
+static const char col_gray1[]       = "#182230";
+static const char col_gray2[]       = "#121a24";
+static const char col_gray3[]       = "#b8a070";
+static const char col_gray4[]       = "#d8a070";
+static const char col_cyan[]        = "#121a24";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "WEB" };
 static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
@@ -27,9 +27,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/*	class					instance		title				tags mask			isfloating	monitor */
+	{		"Gimp",				NULL,				NULL,				0,						1,					-1	},
+	{		"Chromium",		NULL,				NULL,				1 << 8,				0,					-1	},
+	{		"KeepassXC",	NULL,				NULL,				1 << 7,				0,					-1	},
 };
 
 /* layout(s) */
@@ -39,16 +40,17 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 #include "fibonacci.c"
 static const Layout layouts[] = {
-	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
- 	{ "[@]",      spiral },
- 	{ "[\\]",      dwindle },
+	/* first entry is default */
+	/*	symbol		arrange function */
+ 	{		"[@]",		spiral	},
+	{		"[]=",		tile		},
+	{		"[M]",		monocle	},
+ 	{		"[\\]",		dwindle	},
+	{		"><>",		NULL		}, /* no layout function means floating behavior */
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -61,7 +63,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-f", dmenufont, NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
